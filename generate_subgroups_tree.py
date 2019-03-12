b64d837a1dba821166ba77090a7ea7c07bfadd43
Your Real Name Here <Your Email Address Here>
2019-03-12 16:37:08 -0600

    Updated generate_subgroups_tree.py on 2019-03-12. Added functionality to automate generation the subgroups tree. Ability to add outgroup as well as an output directory.

--- generate_subgroups_tree.py
+++ generate_subgroups_tree.py
@@ -1,20 +1,119 @@
 #!/usr/bin/python
+import os
+import sys
+import re
+import csv
+import argparse
+import shutil
+
 from subprocess import Popen, PIPE
 from Bio import Phylo
 
-# Need to set matplotlib to use 'Agg' so that xwindows is not active and gel images can be generated.
-import matplotlib
-matplotlib.use('Agg')
 import networkx, pylab
+
+from Bio import SeqIO
+
+# Path to the mega10 command line package.
+#megacc = '/usr/local/bin/megacc'
+
 megacc = '/usr/bin/megacc';
 
-output_dir = '/var/www/CpnClassiPhyRWebUI/CpnClassiPhyR/db'
-fasta_infile = output_dir + '/' + 'subgroups_tree.fasta'
-mega_align_options_infile = output_dir + '/' + 'clustalw_align.mao'
-mega_align_filename = output_dir + '/' + 'subgroups_clustalw_align'
+# Path to the application files.
+python_path = sys.argv[0]
+app_dir = os.path.dirname(os.path.realpath(python_path))
+#app_dir = "/Users/kmuirhead/Desktop/CpnClassiPhyR/"
+sys.path.append(os.path.abspath(app_dir))
+
+# Path to the mega options files.
+config_dir = os.path.join(app_dir, 'config')
+
+parser = argparse.ArgumentParser()
+
+fasta_infile = None
+outgroups_infile = None
+output_dir = None
+
+parser.add_argument('-i', action='store', dest='fasta_infile',
+                    help='fasta file as input. (i.e. filename.fasta)')
+parser.add_argument('-g', action='store', dest='outgroups_infile',
+                    help='fasta file as input. (i.e. filename.fasta)')
+parser.add_argument('-o', action='store', dest='output_dir',
+                    help='output directory as input. (i.e. $HOME)')
+
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+
+results = parser.parse_args()
+
+fasta_infile = results.fasta_infile
+outgroups_infile = results.outgroups_infile
+output_dir = results.output_dir
+
+if(fasta_infile == None):
+    print('\n')
+    print('error: please use the -i option to specify the fasta file as input')
+    print('fasta_infile =' + ' ' + str(fasta_infile))
+    print('\n')
+    parser.print_help()
+    sys.exit(1)
+if(outgroups_infile == None):
+    print('\n')
+    print('error: please use the -g option to specify the fasta file as input')
+    print('outgroups_infile =' + ' ' + str(outgroups_infile))
+    print('\n')
+    parser.print_help()
+    sys.exit(1)
+if(output_dir == None):
+    print('\n')
+    print('error: please use the -o option to specify the output directory as input')
+    print('output_dir =' + ' ' + str(output_dir))
+    print('\n')
+    parser.print_help()
+    sys.exit(1)
+
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+mega_align_options_infile = os.path.join(config_dir,'clustalw_align.mao')
+mega_align_filename = os.path.join(output_dir,'subgroups_clustalw_align')
+
+mega_tree_filename = os.path.join(output_dir, 'subgroups_mega_tree')
+
+tree_seqsfile = os.path.join(output_dir, 'tree_seqsfile.fasta')
+subgroups_tree_file = os.path.join(output_dir, 'subgroups_tree.fasta')
+
+sequence_ids_outfile = os.path.join(output_dir, 'sequence_ids.csv')
+f = open(sequence_ids_outfile, 'w')
+sequence_ids_output_file = csv.writer(f)
+
+f_out = open(tree_seqsfile, 'w')
+for fasta_record in SeqIO.parse(fasta_infile, "fasta"):
+    seq_id = fasta_record.id
+    description = fasta_record.description
+    fasta_record.description = seq_id
+    sequence_ids_output_file.writerow([seq_id, description, "reference"])
+    
+    r = SeqIO.write(fasta_record, f_out, 'fasta')
+    
+    if(r != 1):
+        print('Error while writing sequence:  ' + seq_record.id)
+
+for fasta_record in SeqIO.parse(outgroups_infile, "fasta"):
+    seq_id = fasta_record.id
+    description = fasta_record.description
+    fasta_record.description = seq_id
+    sequence_ids_output_file.writerow([seq_id, description, "reference"])
+    
+    r = SeqIO.write(fasta_record, f_out, 'fasta')
+    
+    if(r != 1):
+        print('Error while writing sequence:  ' + seq_record.id)
+
+f_out.close()
+f.close()
 
 #megacc -d phylo_phyto_tree.fasta -a clustalw_align.mao -o megacc_test.txt
-megacc_align_cmd = " ".join([megacc, "-d", fasta_infile, "-a", mega_align_options_infile, "-o", mega_align_filename])
+megacc_align_cmd = " ".join([megacc, "-d", tree_seqsfile, "-a", mega_align_options_infile, "-o", mega_align_filename])
 print(megacc_align_cmd)
 p1 = Popen(megacc_align_cmd, stdout=PIPE, stderr=PIPE, shell=True, executable='/bin/bash')
 (megacc_align_results, err) = p1.communicate()
@@ -26,23 +125,23 @@ print(err.decode("utf-8"))
 print(p1_status)
 
 if(p1_status == 0):
-    mega_align_infile = mega_align_filename + '.meg'
-    print(mega_align_infile)
+    mega_align_file = mega_align_filename + '.meg'
+    print(mega_align_file)
 
     #mega_tree_options_infile = '/home/muirheadk/tree_builder_specs.mao'
     #mega_tree_options_infile = '/home/muirheadk/neighbor-joining-tree-params.mao'
     #mega_tree_options_infile = '/home/muirheadk/MP-MEGA.mao'
-    mega_tree_options_infile = output_dir + '/' + 'NJ_nucleotide.mao'
-    mega_tree_filename = output_dir + '/' + 'subgroups_mega_tree'
+    mega_tree_options_infile = os.path.join(config_dir,'NJ_nucleotide.mao')
+    mega_tree_filename = os.path.join(output_dir,'subgroups_mega_tree')
     
     mega_best_tree_infile = mega_tree_filename + '.nwk'
     print(mega_best_tree_infile)
     
-    mega_consensus_tree_infile = mega_tree_filename + '_' + 'consensus.nwk'
-    print(mega_consensus_tree_infile)
-    
+#    mega_consensus_tree_infile = mega_tree_filename + '_' + 'consensus.nwk'
+#    print(mega_consensus_tree_infile)
+
     #megacc -d megacc_test.meg -a tree_builder_specs.mao  -o ML-Test-2018-07-14.txt
-    megacc_tree_cmd = " ".join([megacc, "-d", mega_align_infile, "-a", mega_tree_options_infile, "-o", mega_tree_filename])
+    megacc_tree_cmd = " ".join([megacc, "-d", mega_align_file, "-a", mega_tree_options_infile, "-o", mega_tree_filename])
     print(megacc_tree_cmd)
     p2 = Popen(megacc_tree_cmd, stdout=PIPE, stderr=PIPE, shell=True, executable='/bin/bash')
     (megacc_tree_results, err) = p2.communicate()
@@ -62,20 +161,32 @@ if(p1_status == 0):
         best_tree.root_with_outgroup({'name': 'b30065_LXYB01000004_Acholeplasma_laidlawii_PG8R10'})
         best_tree.rooted = True
         
-        Phylo.draw(best_tree)
-        pylab.savefig(mega_tree_filename + '.png')
+        #Phylo.draw(best_tree)
+        #pylab.savefig(mega_tree_filename + '.png')
         
         Phylo.write(best_tree, mega_tree_filename + '.nwk', 'newick')
         Phylo.write(best_tree, mega_tree_filename + '.xml', 'phyloxml')
         
-        consensus_tree = Phylo.read(mega_consensus_tree_infile, 'newick')
-        print(consensus_tree)
-        
-        consensus_tree.root_with_outgroup({'name': 'b30065_LXYB01000004_Acholeplasma_laidlawii_PG8R10'})
-        consensus_tree.rooted = True
+        f_out = open(subgroups_tree_file, 'w')
+        for fasta_record in SeqIO.parse(tree_seqsfile, "fasta"):
+            seq_id = fasta_record.id
+            fasta_record.description = sequence_header_list[seq_id][0]
+            
+            r = SeqIO.write(fasta_record, f_out, 'fasta')
+            
+            if(r != 1):
+                print('Error while writing sequence:  ' + seq_record.id)
+        f_out.close()
 
-        Phylo.draw(consensus_tree)
-        pylab.savefig(mega_tree_filename + '_' + 'consensus.png')
-        
-        Phylo.write(consensus_tree, mega_tree_filename + '_' + 'consensus.nwk', 'newick')
-        Phylo.write(consensus_tree, mega_tree_filename  + '_' + 'consensus.xml', 'phyloxml')
+
+#        consensus_tree = Phylo.read(mega_consensus_tree_infile, 'newick')
+#        print(consensus_tree)
+#        
+#        consensus_tree.root_with_outgroup({'name': 'b30065_LXYB01000004_Acholeplasma_laidlawii_PG8R10'})
+#        consensus_tree.rooted = True
+#
+#        Phylo.draw(consensus_tree)
+#        pylab.savefig(mega_tree_filename + '_' + 'consensus.png')
+#
+#        Phylo.write(consensus_tree, mega_tree_filename + '_' + 'consensus.nwk', 'newick')
+#        Phylo.write(consensus_tree, mega_tree_filename  + '_' + 'consensus.xml', 'phyloxml')
